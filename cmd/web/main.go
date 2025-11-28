84b69b1d7c1150c8aba3e9efdb25a1cd4aabf4f0
Saeed Ahmed <saeedmedra@gmail.com>
2025-11-28 23:56:12 +0200

    Refactor log into leveled logs

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,11 +4,16 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	addr := flag.String("addr", ": 8888", "PORT")
 	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Lshortfile)
 
 	mux := http.NewServeMux()
 
@@ -19,7 +24,13 @@ func main() {
 	mux.HandleFunc("/note/view", noteView)
 	mux.HandleFunc("/note/create", noteCreate)
 
-	log.Printf("Server Starting on port%s", *addr)
-	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
+	infoLog.Printf("Server Starting on port%s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
