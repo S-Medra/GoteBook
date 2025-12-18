edce417546308632fc72630b754dfaffb2c76f96
Saeed Ahmed <saeedmedra@gmail.com>
2025-12-19 00:12:18 +0200

    Refactor form validation logic into a separate package and integrate it

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,10 +5,9 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
-	"strings"
-	"unicode/utf8"
 
 	"github.com/S-Medra/GoteBook/internal/models"
+	"github.com/S-Medra/GoteBook/internal/validator"
 	"github.com/julienschmidt/httprouter"
 )
 
@@ -62,10 +61,10 @@ func (app *application) noteCreate(w http.ResponseWriter, r *http.Request) {
 }
 
 type noteCreateForm struct {
-	Title       string
-	Content     string
-	Expires     int
-	FieldErrors map[string]string
+	Title   string
+	Content string
+	Expires int
+	validator.Validator
 }
 
 func (app *application) noteCreatePost(w http.ResponseWriter, r *http.Request) {
@@ -83,42 +82,28 @@ func (app *application) noteCreatePost(w http.ResponseWriter, r *http.Request) {
 	}
 
 	form := noteCreateForm{
-		Title:       r.PostForm.Get("title"),
-		Content:     r.PostForm.Get("content"),
-		Expires:     expires,
-		FieldErrors: map[string]string{},
+		Title:   r.PostForm.Get("title"),
+		Content: r.PostForm.Get("content"),
+		Expires: expires,
 	}
 
-	if strings.TrimSpace(form.Title) == "" {
+	form.CheckField(validator.NotBlank(form.Title), "title", "This field cannot be blank")
+	form.CheckField(validator.MaxChars(form.Title, 100), "title", "This field cannot be longer than 100 characters")
+	form.CheckField(validator.NotBlank(form.Content), "content", "This field cannot be blank")
+	form.CheckField(validator.PermittedInt(form.Expires, 1, 7, 365), "expires", "This field must equal 1, 7 or 365")
 
-		form.FieldErrors["title"] = "This field cannot be blank"
-
-	} else if utf8.RuneCountInString(form.Title) > 100 {
-
-		form.FieldErrors["title"] = "This field cannot be more than 100 characters long"
-	}
-
-	if strings.TrimSpace(form.Content) == "" {
-
-		form.FieldErrors["content"] = "This field cannot be blank"
-	}
-
-	if form.Expires != 1 && form.Expires != 7 && form.Expires != 365 {
-
-		form.FieldErrors["expires"] = "This field must equal 1, 7, or 365"
-	}
-
-	if len(form.FieldErrors) > 0 {
+	if !form.Valid() {
 		data := app.newTemplateData(r)
 		data.Form = form
 		app.render(w, http.StatusUnprocessableEntity, "create.html", data)
 		return
 	}
 
-	id, err := app.notes.Insert(form.Title, form.Content, expires)
+	id, err := app.notes.Insert(form.Title, form.Content, form.Expires)
 	if err != nil {
 		app.serverError(w, err)
 		return
 	}
+
 	http.Redirect(w, r, fmt.Sprintf("/note/view/%d", id), http.StatusSeeOther)
 }
