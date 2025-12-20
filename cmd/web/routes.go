249ba08863e3278714cc2ad7788b0b7489c01f64
Saeed Ahmed <saeedmedra@gmail.com>
2025-12-20 21:36:38 +0200

    auth: restricting access to some pages based on login state

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -22,14 +22,16 @@ func (app *application) routes() http.Handler {
 
 	router.Handler(http.MethodGet, "/", dynamic.ThenFunc(app.home))
 	router.Handler(http.MethodGet, "/note/view/:id", dynamic.ThenFunc(app.noteView))
-	router.Handler(http.MethodGet, "/note/create", dynamic.ThenFunc(app.noteCreate))
-	router.Handler(http.MethodPost, "/note/create", dynamic.ThenFunc(app.noteCreatePost))
-
 	router.Handler(http.MethodGet, "/user/signup", dynamic.ThenFunc(app.userSignup))
 	router.Handler(http.MethodPost, "/user/signup", dynamic.ThenFunc(app.userSignupPost))
 	router.Handler(http.MethodGet, "/user/login", dynamic.ThenFunc(app.userLogin))
 	router.Handler(http.MethodPost, "/user/login", dynamic.ThenFunc(app.userLoginPost))
-	router.Handler(http.MethodPost, "/user/logout", dynamic.ThenFunc(app.userLogoutPost))
+
+	protected := dynamic.Append(app.requireAuthentication)
+
+	router.Handler(http.MethodGet, "/note/create", protected.ThenFunc(app.noteCreate))
+	router.Handler(http.MethodPost, "/note/create", protected.ThenFunc(app.noteCreatePost))
+	router.Handler(http.MethodPost, "/user/logout", protected.ThenFunc(app.userLogoutPost))
 
 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 
